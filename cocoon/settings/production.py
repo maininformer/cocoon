8db71a025172b60016776d85ae5d5cde06328c18
emin@berkeley.edu <libra1123>
2016-04-16 01:11:44 -0700

    fix typo in production database URL

--- cocoon/settings/production.py
+++ cocoon/settings/production.py
@@ -6,4 +6,4 @@ import dj_database_url
 DEBUG = False
 ALLOWED_HOSTS = ['cocoon-healthcare.herokuapp.com']
 
-DATABASES={'default':dj_database_url.config(env='CLEAR_DATABASE_URL')}
+DATABASES={'default':dj_database_url.config(env='CLEARDB_DATABASE_URL')}
